433f3a6e4d68556be7adf4a624798e92a8b32b7f
CanadianCommander <ben.benetti@shaw.ca>
2021-03-14 19:42:48 -0700

    remove sudo from prod docker command

--- prod.sh
+++ prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd $(dirname $0)
 
-sudo -E docker-compose -f deploy/rbtgen_prod/docker-compose-rbtgen.yml up
+docker-compose -f deploy/rbtgen_prod/docker-compose-rbtgen.yml up
 
 popd
